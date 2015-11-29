4a50e22e578ca311688f44c5eee695d16eb7556c
Hamid Zade <hamidmamadzade@gmail.com>
2015-11-29 20:52:24 +0000

    confirm plane has landed

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,13 +1,9 @@
 #require_relative 'plane'
 
 class Airport
-	#attr_reader :airport
-
+	
 	def land(plane)
-#		plane
+		plane
 	end
 
-#	def take_off(plane)
-#		plane
-#	end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,10 +1,15 @@
 require 'airport'
 
 describe Airport do 
-	subject(:airport) { described_class.new }	
+	subject(:airport) { described_class.new }
+	let(:plane) { Plane.new }
 
 	it 'Instruct a plane to land' do
 		expect(airport).to respond_to(:land).with(1).argument
+	end
+
+	it 'Land at airport and confirm it has landed' do
+		expect(airport.land(plane)).to eq plane
 	end
 	
 end
